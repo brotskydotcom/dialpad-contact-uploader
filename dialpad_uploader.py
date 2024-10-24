92f59e6f53ed9465d301ad8ffca63caeb9336da5
Daniel Brotsky <dan@brotsky.com>
2024-10-24 12:13:23 -0700

    Update copyrights.

--- dialpad_uploader.py
+++ dialpad_uploader.py
@@ -1,6 +1,6 @@
 #  MIT License
 #
-#  Copyright (c) 2022 Daniel Brotsky
+#  Copyright (c) 2022-2024 Daniel C. Brotsky
 #
 #  Permission is hereby granted, free of charge, to any person obtaining a copy
 #  of this software and associated documentation files (the "Software"), to deal
@@ -22,21 +22,6 @@
 #
 import os.path
 
-#  MIT License
-#
-#  Copyright (c) 2022 Daniel Brotsky
-#
-#  Permission is hereby granted, free of charge, to any person obtaining a copy
-#  of this software and associated documentation files (the "Software"), to deal
-#  in the Software without restriction, including without limitation the rights
-#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#  copies of the Software, and to permit persons to whom the Software is
-#  furnished to do so, subject to the following conditions:
-#
-#  The above copyright notice and this permission notice shall be included in all
-#  copies or substantial portions of the Software.
-#
-#
 import click
 
 from dialpad_contact_uploader.contacts import update_contact_list
